bf60c7d8739f47d170767c4c877cbd8b73d200f4
trdoan <tranduongdoan0922@gmail.com>
2021-08-22 14:49:30 +0700

    update movie datail

---
+++ src/components/RapChieu/RapChieu.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getListHeThongRapAction,
+  getThongTinCumRap,
+} from "../../store/actions/rapChieu.action";
+import rapBHD from "./../../assets/img/bhdrap.png";
+import rapCNS from "./../../assets/img/cnsrap.jpg";
+import rapGalaxy from "./../../assets/img/galaxyrap.jpg";
+import rapCGV from "./../../assets/img/cgvrap.jpg";
+import rapLotte from "./../../assets/img/lotteRap.jpg";
+import rapMega from "./../../assets/img/megarap.jpg";
+function RapChieu() {
+  const dispatch = useDispatch();
+  const heThongRap = useSelector((state) => state.rapChieuReducer.heThongRap);
+  const cumRap = useSelector((state) => state.rapChieuReducer.cumRap);
+  const [rapChieu, setRapChieu] = useState(rapBHD);
+  useEffect(() => {
+    dispatch(getListHeThongRapAction());
+  }, [dispatch]);
+  const handleRenderNavHeader = () => {
+    return heThongRap?.map((rapPhim, index) => {
+      return (
+        <a
+          key={index}
+          className={"nav-link d-flex align-items-center "}
+          data-toggle="pill"
+          href={`#${rapPhim.biDanh}`}
+          role="tab"
+          aria-controls={rapPhim.biDanh}
+          aria-selected="true"
+          onClick={() => handleChonRap(rapPhim)}
+        >
+          <img
+            src={rapPhim.logo}
+            alt={rapPhim.tenHeThongRap}
+            style={{ width: 45, height: 45 }}
+          />
+        </a>
+      );
+    });
+  };
+  const handleRenderNavContent = () => {
+    return cumRap?.map((rap, index) => (
+      <a
+        key={index}
+        href="#"
+        className={
+          index === 0
+            ? "nav-link tab-pane fade show  d-flex active"
+            : "nav-link tab-pane fade show d-flex"
+        }
+        data-toggle="pill"
+        role="tab"
+        aria-controls="#"
+        aria-selected="true"
+        style={{ cursor: "pointer" }}
+      >
+        <img
+          className="hinhAnhCumRap"
+          src={rapChieu}
+          alt={"hinhAnhCumRap"}
+          style={{ width: 95, height: 100, objectFit: "cover" }}
+        />
+        <div className="content">
+          <p> {rap.tenCumRap} </p>
+          <p>{rap.diaChi}</p>
+          <a href="#" style={{ color: "black" }}>
+            Chi tiết
+          </a>
+        </div>
+      </a>
+    ));
+  };
+  const handleChonRap = (rapPhim) => {
+    dispatch(getThongTinCumRap(rapPhim.maHeThongRap));
+    // alert(rapPhim.maHeThongRap);
+    switch (rapPhim.maHeThongRap) {
+      case "BHDStar":
+        return setRapChieu(rapBHD);
+      case "CGV":
+        return setRapChieu(rapCGV);
+      case "CineStar":
+        return setRapChieu(rapCNS);
+      case "Galaxy":
+        return setRapChieu(rapGalaxy);
+      case "LotteCinima":
+        return setRapChieu(rapLotte);
+      case "MegaGS":
+        return setRapChieu(rapMega);
+      default:
+        break;
+    }
+  };
+  return (
+    <div className="heThongRap">
+      <div className="row">
+        <div
+          className="nav flex-column nav-pills"
+          id="v-pills-tab"
+          role="tablist"
+          aria-orientation="vertical"
+        >
+          {handleRenderNavHeader()}
+        </div>
+        <div
+          className="nav flex-column nav-pills"
+          id="v-pills-tab-2"
+          role="tablist"
+          aria-orientation="vertical"
+        >
+          {handleRenderNavContent()}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RapChieu;
--- src/pages/movieDetail/MovieDetail.jsx
+++ src/pages/movieDetail/MovieDetail.jsx
@@ -8,16 +8,8 @@ import Footer from "./../../components/Footer/Footer.component";
 import dateFormat from "dateformat";
 
 import "./MovieDetail.scss";
-import {
-  getListHeThongRapAction,
-  getThongTinCumRap,
-} from "../../store/actions/rapChieu.action";
-// import rapBHD from "./../../img/bhdrap.png";
-// import rapCNS from "./../../img/cnsrap.jpg";
-// import rapGalaxy from "./../../img/galaxyrap.jpg";
-// import rapCGV from "./../../img/cgvrap.jpg";
-// import rapLotte from "./../../img/lotteRap.jpg";
-// import rapMega from "./../../img/megarap.jpg";
+
+import RapChieu from "../../components/RapChieu/RapChieu";
 
 function MovieDetail() {
   const { maPhim } = useParams();
@@ -25,77 +17,23 @@ function MovieDetail() {
   useEffect(() => {
     dispatch(getDetailMovieAction(maPhim));
   }, [dispatch, maPhim]);
-  useEffect(() => {
-    dispatch(getListHeThongRapAction());
-  }, [dispatch]);
+
   const movieDetail = useSelector((state) => state.movieReducer.movieDetail);
   const isLoading = useSelector((state) => state.commonReducer.isLoading);
-  const { tenPhim, trailer, hinhAnh, ngayKhoiChieu, lichChieu, danhGia } =
-    movieDetail;
-  const heThongRap = useSelector((state) => state.rapChieuReducer.heThongRap);
-  const cumRap = useSelector((state) => state.rapChieuReducer.cumRap);
+  const {
+    tenPhim,
+    trailer,
+    hinhAnh,
+    ngayKhoiChieu,
+    lichChieu,
+    danhGia,
+  } = movieDetail;
+
   // lấy thông tin chi tiết phim lịch chiếu,...
 
   if (lichChieu) {
     var { thoiLuong } = movieDetail.lichChieu[0];
   }
-
-  const handleChonRap = (rapPhim) => {
-    dispatch(getThongTinCumRap(rapPhim.maHeThongRap));
-  };
-  const handleRenderNavHeader = () => {
-    return heThongRap?.map((rapPhim, index) => {
-      return (
-        <a
-          key={index}
-          className={"nav-link d-flex align-items-center "}
-          data-toggle="pill"
-          href={`#${rapPhim.biDanh}`}
-          role="tab"
-          aria-controls={rapPhim.biDanh}
-          aria-selected="true"
-          onClick={() => handleChonRap(rapPhim)}
-        >
-          <img
-            src={rapPhim.logo}
-            alt={rapPhim.tenHeThongRap}
-            style={{ width: 45, height: 45 }}
-          />
-        </a>
-      );
-    });
-  };
-  const handleRenderNavContent = () => {
-    return cumRap?.map((rap, index) => (
-      <a
-        key={index}
-        href="#"
-        className={
-          index === 0
-            ? "nav-link tab-pane fade show  d-flex active"
-            : "nav-link tab-pane fade show d-flex"
-        }
-        data-toggle="pill"
-        role="tab"
-        aria-controls="#"
-        aria-selected="true"
-        style={{ cursor: "pointer" }}
-      >
-        <img
-          // src={imgRap}
-          alt=""
-          style={{ width: 95, height: 100, objectFit: "cover" }}
-        />
-        <div className="content">
-          <p> {rap.tenCumRap} </p>
-          <p>{rap.diaChi}</p>
-          <a href="#" style={{ color: "black" }}>
-            Chi tiết
-          </a>
-        </div>
-      </a>
-    ));
-  };
 
   return isLoading ? (
     <Loading />
@@ -139,26 +77,7 @@ function MovieDetail() {
           </div>
         </div>
         {/* BUG */}
-        <div className="heThongRap">
-          <div className="row">
-            <div
-              className="nav flex-column nav-pills"
-              id="v-pills-tab"
-              role="tablist"
-              aria-orientation="vertical"
-            >
-              {handleRenderNavHeader()}
-            </div>
-            <div
-              className="nav flex-column nav-pills"
-              id="v-pills-tab-2"
-              role="tablist"
-              aria-orientation="vertical"
-            >
-              {handleRenderNavContent()}
-            </div>
-          </div>
-        </div>
+        <RapChieu />
       </div>
       <Footer />
     </>
