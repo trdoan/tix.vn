0270aab5ab25fd4b8551a5d5346ef8c245776b79
doanvipqua <tranduongdoan0922@gmail.com>
2021-08-29 15:51:55 +0700

    test error page

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import hinh from "./../../assets/img/404.jpg";
 import "./Error.scss";
 function ErrorPage() {
   return (
-    <div className="pageError">
-      <div className="container text-center">
-        <div className="row align-items-center">
-          <div className="col-12 col-md-7 imgContainer">
-            <img src={hinh} alt="404page" className="imgError img-fluid" />
+    <section className="page_404">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <div className="col-sm-10 col-sm-offset-1  text-center">
+              <h1 className="text-center ">Opps 404 :(</h1>
+              <div
+                className="four_zero_four_bg"
+                style={{ marginTop: "50px" }}
+              ></div>
+              <div className="contant_box_404 ">
+                <h3 className="h2">Bạn ơi!....Có gì đó sai sai ?</h3>
+                {/* <p>the page you are looking for not avaible!</p> */}
+                <Link to="/" className="link_404">
+                  <span className="text"> TRANG CHỦ</span>
+                </Link>
+              </div>
+            </div>
           </div>
-          <h2 className="col-12 col-md-5 contentError">
-            OOPS! TRANG KHÔNG TỒN TẠI
-          </h2>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
